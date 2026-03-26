cf3244f28ccb689a72db87f551d5d018df56a707
One4Shell <h4shell@protonmail.com>
2026-03-26 14:07:43 +0100

    feat: Update hero video to new content
    Replaces the embedded YouTube video in the HeroVideoBackground component with a new video URL. This change updates the visual content of the hero section.

--- src/components/HeroVideoBackground.tsx
+++ src/components/HeroVideoBackground.tsx
@@ -7,7 +7,7 @@ export const HeroVideoBackground: React.FC = () => {
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[115%] h-[115%] md:w-[100vw] md:h-[56.25vw] min-h-[100vh] min-w-[177.77vh]">
         <iframe
           className="w-full h-full"
-          src="https://www.youtube-nocookie.com/embed/hOgVAYpHPCc?autoplay=1&mute=1&loop=1&playlist=hOgVAYpHPCc&controls=0&showinfo=0&autohide=1&modestbranding=1&rel=0&disablekb=1&fs=0&playsinline=1&iv_load_policy=3&enablejsapi=1"
+          src="https://www.youtube-nocookie.com/embed/S2kymv60ndQ?autoplay=1&mute=1&loop=1&playlist=S2kymv60ndQ&controls=0&showinfo=0&autohide=1&modestbranding=1&rel=0&disablekb=1&fs=0&playsinline=1&iv_load_policy=3&enablejsapi=1"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         />
